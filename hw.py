caf6ac1b4d18edc5cf6c0fe3c2dbc6b0829afb0b
Aroosh Ali <arooshalisharif@gmail.com>
2025-01-09 22:33:11 +0500

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,7 @@
+#Making guessing game
+print("Guess a number .If its big then the number i choose you lose ❌ if its smaller you win 👑")
+Num = int(input("Guess a Number:"))
+if Num>67:
+    print("YOU LOSE 😥 Sorry!Your answer is bigger then value.Play Again")
+else:
+    print("YOU WIN 👑.Your guess is right.")
